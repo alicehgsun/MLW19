92ffdec89ec139b9013bb96fda7e91776240a4c6
Alice Sun <hgs266@nyu.edu>
2019-05-08 14:11:56 -0400

    typo

--- final/collect/call-json.py
+++ final/collect/call-json.py
@@ -2,16 +2,12 @@ import requests
 import json
 from requests_oauthlib import OAuth1
 
-
 for x in range(0, 22):
     auth = OAuth1("KEY", "SECRET_KEY")
-    # endpoint = "https://api.thenounproject.com/icons/doughnut?page=2"
     endpoint = "https://api.thenounproject.com/icons/doughnut?page=" + str(x)
 
     response = requests.get(endpoint, auth=auth)
     # print (response.content)
-
-
 
     with open('./jsons/doughnut-p' + str(x) + '.json', 'w') as results_file:
         json.dump(response.json(), results_file)
--- final/collect/json-to-image.py
+++ final/collect/json-to-image.py
@@ -2,12 +2,11 @@ import urllib.request as ur
 import json
 import sys
 
-# for x in range(10, 20):
+# for x in range(0, 20):
 #     with open ('./doughnut-p' + str(x) + '.json') as data_file:
 #         icon_data = json.load(data_file)["icons"]
 #         for item in icon_data:
 #             ur.urlretrieve(item["preview_url"], "./"+ item["id"] + ".png")
-
 
 import requests
 import shutil
